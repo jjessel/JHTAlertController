f2c7f57d67a56e02788354a78ad2154066466f48
jjessel <jjesse84@gmail.com>
2016-11-21 13:49:37 -0800

    Added carthage support

---
+++ Example/JHTAlertController/JHTAlertController.h
@@ -0,0 +1,19 @@
+//
+//  JHTAlertController.h
+//  JHTAlertController
+//
+//  Created by Jessel, Jeremiah on 11/21/16.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JHTAlertController.
+FOUNDATION_EXPORT double JHTAlertControllerVersionNumber;
+
+//! Project version string for JHTAlertController.
+FOUNDATION_EXPORT const unsigned char JHTAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JHTAlertController/PublicHeader.h>
+
+
